dcbeef31c2e5baed78fcc179d892cbedff37593d
Tharindu Kalhara <tharindukalhara73@gmail.com>
2026-05-23 18:30:21 +0530

    Update ECS greeting to reflect the current date

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('/', (req, res) => {
 </head>
 <body>
   <div class="card">
-    <h1>🚀 Hello from ECS 20/05!</h1>
+    <h1>🚀 Hello from ECS 23/05!</h1>
     <p>Your pipeline is working perfectly.</p>
   </div>
 </body>
